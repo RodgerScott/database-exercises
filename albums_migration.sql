917993d6fe9aa80edbca01ced5151c0ca4b40f4d
RodgerScott <rodger.scott@gmail.com>
2018-03-16 12:02:03 -0500

    removed a bunch of DEFAULT NULLS

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100) DEFAULT 'NONE',
-  name VARCHAR(100) DEFAULT NULL,
+  artist VARCHAR(100),
+  name VARCHAR(100),
   release_date INT UNSIGNED DEFAULT NULL,
-  sales FLOAT UNSIGNED DEFAULT NULL,
-  genre VARCHAR (25) DEFAULT NULL,
+  sales DECIMAL(10,2),
+  genre VARCHAR (25),
   PRIMARY KEY (id)
 );
 
@@ -16,9 +16,9 @@ DROP TABLE IF EXISTS contacts;
 
 CREATE TABLE contacts (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR (50) NOT NULL,
-  number VARCHAR (15) NOT NULL,
-  email VARCHAR (50) NOT NULL,
+  name VARCHAR (50),
+  number VARCHAR (15),
+  email VARCHAR (50),
   created_at DATETIME,
   updated_at DATETIME,
   PRIMARY KEY (id)
@@ -29,11 +29,11 @@ DROP TABLE IF EXISTS pizza_order_table;
 CREATE TABLE pizza_order_table (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(50),
-  phone_number VARCHAR (15) NOT NULL,
+  phone_number VARCHAR (15),
   email VARCHAR (50),
   pizza_size CHAR (2),
   total_cost DECIMAL(5,2),
-  description TEXT DEFAULT NULL,
+  description TEXT,
   isDelivered BOOLEAN,
   created_at DATETIME,
   updated_at DATETIME,
