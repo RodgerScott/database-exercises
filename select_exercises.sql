97f04bd6096ddbac2ee2ddf2d18caeaef70e7c00
RodgerScott <rodger.scott@gmail.com>
2018-03-16 16:55:17 -0500

    working on exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,13 +1,12 @@
 USE codeup_test_db;
 
-SELECT album AS Pink_Floyd_Albums_Names from albums where artist = 'Pink Floyd';
+SELECT album AS 'Pink Floyd Album Names' from albums where artist = 'Pink Floyd';
 
-SELECT Album, release_date AS Release_Date from albums where Album = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'The Year Sgt Pepper Was Released' from albums where Album = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT Album, artist AS Genre, artist from albums where album = 'Nevermind';
+SELECT genre AS 'Genre Of Nirvana''s Nevermind' from albums where album = 'Nevermind';
 
 
--- SELECT genre from albums where album = 'Nevermind';
 --
 -- SELECT album from albums where release_date > 1990 && release_date < 2000;
 --
