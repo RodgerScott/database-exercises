6ce4450e0e0b1e6603587e374e6fb4196a82838a
RodgerScott <rodger.scott@gmail.com>
2018-03-17 21:27:16 -0500

    completed select portion of exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,19 +2,14 @@ USE codeup_test_db;
 
 SELECT album AS 'Pink Floyd Album Names' from albums where artist = 'Pink Floyd';
 
-SELECT release_date AS 'The Year Sgt Pepper Was Released' from albums where Album = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'The Year Sgt Pepper Was Released' from albums where album = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre AS 'Genre Of Nirvana''s Nevermind' from albums where album = 'Nevermind';
 
+SELECT album AS 'Albums released between 1990 and 2000' from albums WHERE release_date BETWEEN 1990 and 1999;
 
---
--- SELECT album from albums where release_date > 1990 && release_date < 2000;
---
--- SELECT album from albums where sales < 20;
---
--- select album from albums where genre = 'rock';
+SELECT album AS 'Albums with less than 20 million certified sales' from albums WHERE sales < 20;
 
-
-
+SELECT album AS 'Rock Albums' from albums WHERE genre = 'rock';
 
 
