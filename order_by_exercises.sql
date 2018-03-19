968e51ecba3f2df6c6a8e42555264b8104466a5e
RodgerScott <rodger.scott@gmail.com>
2018-03-19 16:59:36 -0500

    completed Order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,66 @@
+USE employees;
+
+# Exercise 2 (revisit)
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+# Exercise 3
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name ASC , last_name ASC;
+
+# Exercise 4
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC , first_name ASC;
+
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'm';
+
+
+# Exercise 5
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no ASC;
+
+# Exercise 6
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%-%-%';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+# Exercise 7
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%-%-%' AND birth_date LIKE '%-12-25'
+ORDER BY hire_date DESC, birth_date DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
