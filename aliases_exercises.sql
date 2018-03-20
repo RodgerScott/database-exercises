31033fa9e24ed81b0cc430eccfff5728da194e06
RodgerScott <rodger.scott@gmail.com>
2018-03-20 16:34:37 -0500

    completed Aliases exercise

---
+++ aliases_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+# Exercise 3
+
+SELECT CONCAT(last_name, ', ', first_name) AS 'full_name'
+FROM employees
+LIMIT 10;
+
+# Exercise 4
+
+SELECT CONCAT(last_name, ', ', first_name) AS 'full_name', CONCAT(birth_date) AS 'DOB'
+FROM employees
+LIMIT 10;
+
+# Exercise 5
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS 'full_name', CONCAT(birth_date) AS 'DOB'
+FROM employees
+LIMIT 10;
