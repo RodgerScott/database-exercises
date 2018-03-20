5637e5977c2a8dfa1ae20ab3d3a5b1bcaef00f28
RodgerScott <rodger.scott@gmail.com>
2018-03-20 09:46:54 -0500

    completed exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -59,7 +59,7 @@ WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 SELECT *
 FROM employees
 WHERE hire_date LIKE '199%-%-%' AND birth_date LIKE '%-12-25'
-ORDER BY hire_date DESC, birth_date DESC;
+ORDER BY birth_date, hire_date DESC;
 
 SELECT *
 FROM employees
