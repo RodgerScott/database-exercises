7ff1dfac485305dfdddc21bdb8f1aaabd65ec3ba
RodgerScott <rodger.scott@gmail.com>
2018-03-22 22:13:59 -0500

    working on bonus drills

--- relationship_drills.sql
+++ relationship_drills.sql
@@ -1,5 +1,27 @@
 USE employees;
 
+#================================= RELATIONSHIP DRILLS
+-- concat the max salaries of the first two employees
+-- get current manager name of Sales department (use subqueries)
+-- get all senior engineers in customer service department
+-- get all first and last names of current Sales department employees
+-- get the name of the employee with the highest salary
+#================================= RELATIONSHIP BONUSES
+-- what is the most common birthday in the company? least common?
+-- what is the average salary of managers by department?
+-- how many employees currently work in each department?
+-- what is the average salary for each department?
+-- what was the average salary in the 80s? By department?
+-- how many times (on average) do employees switch titles?
+-- how many times (on average) do employees get a raise?
+-- what is the highest paying job title historically for women and men? For women only? For men only?
+-- does the older halve of current employees make more or less than the younger halve?
+-- list the oldest current employees by title in each department
+-- list the 5 historically lowest paid managers
+-- EXTRA HARD BONUS --
+-- what employees were hired during the year Nevermind was released and after?
+#(you will need to use the codeup_test_db and employees db)
+
 -- 1. concat the max salaries of the first two employees
 
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS First_2_Employees, salary
@@ -19,3 +41,14 @@ ON e.emp_no = dep.emp_no
 JOIN departments part
 ON dep.dept_no = part.dept_no
 WHERE part.dept_name LIKE 'SALES' AND t.title = 'Manager' AND dep.to_date > curdate();
+
+SELECT DISTINCT e.first_name, e.last_name, manager.to_date
+FROM employees e
+JOIN dept_manager manager ON e.emp_no = manager.emp_no
+JOIN departments d ON manager.dept_no = d.dept_no
+WHERE d.dept_name IN (
+  SELECT d.dept_name
+  FROM departments
+  WHERE manager.to_date = '9999-01-01' AND d.dept_name = 'sales'
+);
+
